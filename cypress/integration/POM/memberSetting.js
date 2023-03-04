6bad5d1feb8489e1626ae4a48d08f2ceec9692ec
ibraz511 <fatai.ibrahim@babbangona.com>
2023-03-04 15:13:37 +0100

    Completed Member Settings Menu

--- cypress/integration/POM/memberSetting.js
+++ cypress/integration/POM/memberSetting.js
@@ -7,19 +7,37 @@ describe('Member Settings Menu', () => {
     })
 
     it('Select Rice and Seed type',()=>{
-        cy.get('.dropdown-heading').click()
+        cy.get('div.dropdown-heading-value').click()
         cy.contains('Rice').click() // select rice
 
+        //unselect maize since it is selected by default
+        cy.contains('Maize').click() // unselect Maize
+        
         cy.get(':nth-child(2) > .rmsc > .dropdown-container') .click() //selecting seed type for rice
     }) 
 
-    it('Select Maize and Seed type',()=>{
-        cy.get('.dropdown-heading').click()
-        cy.contains('Maize').click() // select maize
-        cy.get(':nth-child(2) > .rmsc > .dropdown-container') // selecting seed type for maize
+    it.only('Select Maize and Seed type',()=>{
+       //Maize is selected by default.
+
+       //SM15-F seed type is selected by default, lets select PVA13B and unselect SM15-F
+        
+        // cy.contains('SM15-F').click() // unselect SM15-F
+        cy.get(':nth-child(2) > .rmsc > .dropdown-container > .dropdown-heading > .clear-selected-button > .dropdown-search-clear-icon').click()// click the clear button
+        cy.get('div.dropdown-heading-value>span').click({multiple:true}) // selecting seed type for maize
+        cy.contains('PVA13B').click() // select PVA13B
+
+        cy.get('.css-319lph-ValueContainer > .css-6j8wv5-Input').click({force:true})
+        cy.get('#react-select-7-option-1').click() // select logistics for forecast group
+
+
+        cy.get('.css-319lph-ValueContainer > .css-6j8wv5-Input').click({force:true}) //selecting logistics category
+        cy.get('#react-select-9-option-0').click() //selecting transporter
+
+        //clicking the update button
+        cy.get('.css-oev5bd > .MuiButton-root').click()
     }) 
 
-    it.only('Download and Upload Templates',()=>{
+    it.skip('Download and Upload Templates',()=>{
         
         //Downloading TG and TGE templates
         
