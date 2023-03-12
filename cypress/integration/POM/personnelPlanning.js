82be379548165195a3b214ed3bc5fe38c156c4a6
ibraz511 <fatai.ibrahim@babbangona.com>
2023-03-12 13:42:31 +0100

    Completed personnel planning

---
+++ cypress/integration/POM/personnelPlanning.js
@@ -0,0 +1,47 @@
+export class personnel{
+    visitPersonnel(){
+        cy.contains('Personnel planning').click({force:true});
+    }
+    teamStafing(){
+
+    }
+    advisorSupervisor(){
+
+    }
+
+    salariesAdvisor(){
+
+    }
+
+    directCoachSup(){
+
+    }
+
+    TGEcoachSup(){
+
+    }
+    salariesCoach(){
+
+    }
+    harvestAdvisor(){
+
+    }
+    inputDistribution(){
+
+    }
+    distributionCenterSalary(){
+
+    }
+    inventoryControlTeam(){
+
+    }
+    harvestCollection(){
+
+    }
+    inventoryControlTeam_harvest(){
+
+    }
+    shipmentTeam(){
+        
+    }
+}
---
+++ cypress/integration/personnelPlanning.spec.js
@@ -0,0 +1,40 @@
+///<reference types="cypress"/>
+import { loginPage } from "./POM/loginPage";
+import { personnel } from "./POM/personnelPlanning";
+
+describe("Location Planning", () => {
+  let credentials;
+  const my_login_page = new loginPage();
+  const person = new personnel();
+  
+  // importing the stored username and password
+  before(() => {
+    cy.fixture("Login_credentials").then((data) => {
+      credentials = data;
+    });
+  });
+
+  beforeEach(() => {
+    my_login_page.navigate("/");
+    my_login_page.validLogin(credentials.username, credentials.password);
+    person.visitPersonnel();
+  });
+  
+    
+  it('Fill personnel planing details',()=>{
+   person.teamStafing()
+   person.advisorSupervisor()
+   person.salariesAdvisor()
+   person.directCoachSup()
+   person.TGEcoachSup()
+   person.salariesCoach()
+   person.harvestAdvisor()
+   person.inputDistribution()
+   person.distributionCenterSalary()
+   person.inventoryControlTeam()
+   person.harvestCollection()
+   person.inventoryControlTeam_harvest()
+   person.shipmentTeam()
+})
+
+})
