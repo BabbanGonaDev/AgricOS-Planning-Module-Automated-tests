68cd7c1531cbf94c8ad6385a3ea258de5ede3e05
ibraz511 <fatai.ibrahim@babbangona.com>
2023-03-12 13:16:03 +0100

    completed location planning

--- cypress/integration/POM/locationPlanning.js
+++ cypress/integration/POM/locationPlanning.js
@@ -3,6 +3,10 @@ export class location{
         cy.contains('Location planning').click({force:true});
     }
     fillDetails(){
-        cy.get('#tgeOption')
-        }
+        cy.get('#tgeOption').click({force:true})
+        cy.get('[data-value="yes"]').click({force:true})
+        cy.get('#tg-retention-rate').type('40',{force:true})
+        cy.get(':nth-child(2) > :nth-child(8) > .css-pa7wff > .MuiButton-root').click()
+        cy.get(':nth-child(1) > .css-1rgwm5s-MuiTypography-root').should('have.text', 'View Program')    
+    }
 }
